dc2a64361f31fca1d16044d5ef0b669eaca51a4e
Dominik <dominik@dominikschuermann.de>
2012-08-24 15:26:11 +0200

    add nativetools (attention: this is gpl code!)

---
+++ RootCommands Demo/jni/Application.mk
@@ -0,0 +1,9 @@
+# Optimizations
+APP_OPTIM := release
+
+# Build target
+APP_ABI := armeabi armeabi-v7a x86 mips
+#APP_ABI := armeabi armeabi-v7a x86 mips
+
+# If APP_MODULES is not set, all modules are compiled!
+APP_MODULES := nativetools
