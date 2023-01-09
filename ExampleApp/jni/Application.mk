838a3116d03c8c9420a3731c80fb964485335d83
anticop <61539323+anticop@users.noreply.github.com>
2023-01-09 22:21:42 +0000

    ied:   ExampleApp/.gitignore

--- ExampleApp/jni/Application.mk
+++ ExampleApp/jni/Application.mk
@@ -6,4 +6,5 @@ APP_ABI := armeabi x86 mips
 #APP_ABI := armeabi armeabi-v7a x86 mips
 
 # If APP_MODULES is not set, all modules are compiled!
-APP_MODULES := hello_world
+APP_MODUAPP_MODULES := hello_world
+LES := hello_world
